d6dfd19a7a4228931afea91ad9a9217996dd0a35
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-02-10 13:46:18 -0800

    fix the proc name code to work in tests

--- lib/Nomo/Server.pm
+++ lib/Nomo/Server.pm
@@ -85,18 +85,18 @@ sub pre_loop_hook {
     }) if $self->{options}{server_ready};
 
     register_sig(
-        TTIN => sub { my $p = $self->{server}; $p->{$_}++ for qw( min_servers max_servers ) },
-        TTOU => sub { my $p = $self->{server}; $p->{$_}-- for qw( min_servers max_servers ) },
+        TTIN => sub { $self->{server}->{$_}++ for qw( min_servers max_servers ) },
+        TTOU => sub { $self->{server}->{$_}-- for qw( min_servers max_servers ) },
     );
 
-    $0 = "nomo master " . join(" ", @{$self->{options}{argv}});
+    $0 = "nomo master " . join(" ", @{$self->{options}{argv} || []});
 }
 
 # The below methods run in the child process
 
 sub child_init_hook {
     my $self = shift;
-    $0 = "nomo worker " . join(" ", @{$self->{options}{argv}});
+    $0 = "nomo worker " . join(" ", @{$self->{options}{argv} || []});
 }
 
 sub post_accept_hook {
