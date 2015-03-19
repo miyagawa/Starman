d852eae4d2c844f7b88c97eece2c6ea876dacb8f
Jeremy Krieg <Jeremy.Krieg@YourAmigo.com>
2015-03-19 13:16:20 +1030

    Fixed warnings about modifying a hash while iterating over it in Plack::Handler::Starman.

--- lib/Plack/Handler/Starman.pm
+++ lib/Plack/Handler/Starman.pm
@@ -15,10 +15,12 @@ sub run {
         @Starman::Server::ISA = qw(Net::Server::SS::PreFork); # Yikes.
     }
 
+    my %nsa;
     while (my($key, $value) = each %$self) {
         $key =~ s/^net_server_// or next;
-        $self->{net_server_args}{$key} = $value;
+        $nsa{$key} = $value;
     }
+    $self->{net_server_args} = \%nsa if %nsa;
 
     Starman::Server->new->run($app, {%$self});
 }
